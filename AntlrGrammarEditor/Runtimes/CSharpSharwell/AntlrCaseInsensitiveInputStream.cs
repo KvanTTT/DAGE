ce88dea02f60fcbfdcad6a7637b4126e34d456e0
Ivan Kochurkin <kvanttt@gmail.com>
2016-09-15 20:54:09 +0300

    Added missed files.

---
+++ AntlrGrammarEditor/Runtimes/CSharpSharwell/AntlrCaseInsensitiveInputStream.cs
@@ -0,0 +1,39 @@
+﻿using Antlr4.Runtime;
+
+class AntlrCaseInsensitiveInputStream : AntlrInputStream
+{
+    private string lookaheadData;
+
+    public readonly bool LowerCase;
+
+    public AntlrCaseInsensitiveInputStream(string input, bool lowerCase = true)
+        : base(input)
+    {
+        LowerCase = lowerCase;
+        lookaheadData = lowerCase ? input.ToLower() : input.ToUpper();
+    }
+
+    public override int La(int i)
+    {
+        if (i == 0)
+        {
+            return 0; // undefined
+        }
+        if (i < 0)
+        {
+            i++; // e.g., translate LA(-1) to use offset i=0; then data[p+0-1]
+            if (p + i - 1 < 0)
+            {
+                return IntStreamConstants.Eof; // invalid; no char before first char
+            }
+        }
+
+        if (p + i - 1 >= n)
+        {
+            return IntStreamConstants.Eof;
+        }
+
+        return lookaheadData[p + i - 1];
+    }
+}
+
