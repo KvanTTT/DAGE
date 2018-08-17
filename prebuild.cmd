323326bbe45261f3e02ac4e4d8741803e206b0df
Ivan Kochurkin <kvanttt@gmail.com>
2018-08-17 15:32:49 +0300

    Remove prebuild.cmd (no need)

--- prebuild.cmd
+++
@@ -1,4 +0,0 @@
-cd AntlrGrammarEditor
-java -jar "Generators/antlr-4.6.4-csharpsharwell.jar" "ANTLRv4Lexer.g4" -o "Generated" -Dlanguage=CSharp_v4_5 -package AntlrGrammarEditor
-java -jar "Generators/antlr-4.6.4-csharpsharwell.jar" "ANTLRv4Parser.g4" -o "Generated" -Dlanguage=CSharp_v4_5 -package AntlrGrammarEditor
-cd ..
